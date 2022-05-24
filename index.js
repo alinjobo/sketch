38462ec0c37b856bce21816773646d28a4a4a456
Alin Jobo <alinjob@gmail.com>
2022-05-24 07:39:26 +0530

    changed div[] to div

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 function creategrid(size=16){
+    //const div=[];
     
+    const cont=document.querySelector('.container');
+   
     const s=size*size;
-    const side= 600/size;
+    const side= 960/size;
     console.log(side);
+    
     for(let i=0;i<s;++i){
-        div[i]=document.createElement('div');
-        div[i].classList.add('pixel');
+      const  div=document.createElement('div');
+        div.classList.add('pixel');
         
-        div[i].style.cssText= `width:${side}px; height:${side}px`;
-      //  div[i].style.height="37.5px";
-       // div[i].style.width="37.5px";
-        cont.appendChild(div[i]);
+        div.style.cssText= `width:${side}px; height:${side}px`;
+    
+        cont.appendChild(div);
         
        cont.addEventListener('mousedown',()=>{
-        div[i].addEventListener('mouseover',()=>{
-            div[i].style.backgroundColor= 'black';
+        div.addEventListener('mouseover',()=>{
+            div.style.backgroundColor= 'black';
             
         });
        });
-      cont.addEventListener('mouseup',()=>{
-        div[i].removeEventListener();
+      cont.addEventListener('mouserelease',()=>{
+        div.removeEventListener();
       });
        // cont.addEventListener('')
        
@@ -28,15 +31,15 @@ function creategrid(size=16){
 
 
 }
-const div=[];
-const cont=document.querySelector('.container');
+
 creategrid();
 
 const b=document.querySelector('button');
 function newgrid()
 {
+      
+      creategrid(prompt('Enter size'));
         
-        creategrid(prompt(''));
 }
 b.addEventListener('click',newgrid);
 //
